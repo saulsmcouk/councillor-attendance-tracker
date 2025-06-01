d8ec332cf9f76981c68a818e395f84a847ffcd94
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2025-06-01 12:38:22 +0100

    Initial commit

---
+++ scrapers/durham.js
@@ -0,0 +1,9 @@
+// This is for councils who expose attendance 
+
+const councillorPageUrls = {
+    "Durham City Council": "https://democracy.durham.gov.uk/mgMemberIndex.aspx?VW=TABLE&PIC=1&FN="
+}
+
+async function getCouncillorUrls(council = "Durham") {
+
+}
