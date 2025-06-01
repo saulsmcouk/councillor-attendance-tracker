46b4f6ada87fab56956516c631021e203f85bc50
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2025-06-01 12:41:50 +0100

    Ignore node_modules!

--- scrapers/durham.js
+++ scrapers/durham.js
@@ -1,4 +1,6 @@
 // This is for councils who expose attendance 
+import cheerio from 'cheerio'
+
 
 const councillorPageUrls = {
     "Durham City Council": "https://democracy.durham.gov.uk/mgMemberIndex.aspx?VW=TABLE&PIC=1&FN="
